4899c87eea9fd0756e541a5cc26ba00f0f0f8ebe
Samuel <sharris163@west-mec.org>
2021-02-23 14:15:29 -0700

    remove pug installed ejs

--- faker.js
+++ faker.js
@@ -1,5 +1,5 @@
 var faker = require('faker');
 
-    var randomName = faker.name.findName(); // Rowan Nikolaus
-    var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-    var randomCard = faker.helpers.createCard(); // random contact card containing many properties
+    var personReport = faker.name.findName(); // Rowan Nikolaus
+    var incidentReport = faker.internet.email(); // Kassandra.Haley@erich.biz
+    var vehicleReport = faker.helpers.createCard(); // random contact card containing many properties
