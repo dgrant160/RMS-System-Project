ad66c992be1d7ccbe6a9a214c1a1414560c34dd5
Dylan <dgrant60@west-mec.org>
2021-03-09 13:33:59 -0700

    added form function in the backend

--- controllers/incidentController.js
+++ controllers/incidentController.js
@@ -1,33 +1,69 @@
 var Vehicle = require('../models/vehicle');
 var async = require('async');
 var Person = require('../models/person')
-exports.index = function (req, res) {
-    res.render('testing');
-}
+// exports.index = function (req, res) {
+//     res.render('testing');
+// }
 exports.incident_create_get = function (req, res, next) {
-    async.parallel({
-        Person: function (callback) {
-            Person.find(callback);
-        },
-        Vehicle: function (callback) {
-            Vehicle.find(callback);
-        },
-    }, function (err, results) {
+     async.parallel({
+         Person: function (callback) {
+             Person.find(callback);
+         },
+         Vehicle: function (callback) {
+             Vehicle.find(callback);
+         },
+     }, function (err, results) {
         if (err) { return next(err); }
         res.render('incident_form', { title: 'Create Incident' });
     });
 };
-exports.incident_detail = function (req, res, next) {
-    Person.findOne({
-        Person: function (callback) {
-            Person.find(callback);
-        },
-        Vehicle: function (callback) {
-            Vehicle.find(callback);
-        },
-    }, function (err, results) {
-        if (err) { return next(err); }
-        //console.log(results.vehicle)
-        res.render('incident_detail', { title: 'Incident detail' });
-    });
-};
+// exports.incident_detail = function (req, res, next) {
+//     Person.findOne({
+//         Person: function (callback) {
+//             Person.find(callback);
+//         },
+//         Vehicle: function (callback) {
+//             Vehicle.find(callback);
+//         },
+//     }, function (err, results) {
+//         if (err) { return next(err); }
+//         //console.log(results.vehicle)
+//         res.render('incident_detail', { title: 'Incident detail' });
+//     });
+// };
+exports.incident_create_post = [
+
+    (req, res, next) => {
+
+        var incident = new Incident(
+            {
+                irn: req.body.irn,
+                date: req.body.date,
+                location_info: req.body.location_info,
+                location_name: req.body.location_name,
+                narrative: req.body.narrative,
+                officer: req.body.officer,
+                badge_number: req.body.badge_number,
+                date_of_birth: req.body.date_of_birth,
+                report_date: req.body.report_date,
+                type: req.body.type,
+                related_person: req.body.related_person,
+                related_vehicle: req.body.related_vehicle,
+            }
+        );
+
+        if (!errors.isEmpty()) {
+            
+            res.render('incident_form', { title: 'Create Incident', incident: incident, errors: errors.array() });
+            return;
+        }
+        else {
+            
+            incident.save(function (err) {
+                if (err) { return next(err); }
+                
+                res.redirect(incident.url);
+            });
+        }
+    }
+];
--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -13,3 +13,38 @@ exports.vehicle_create_get = function (req, res, next) {
         res.render('vehicle_form', { title: 'Create Vehicle' });
 //     });    
 };
+
+exports.vehicle_create_post = [
+
+    (req, res, next) => {
+
+        var vehicle = new Vehicle(
+            {
+                license: req.body.license,
+                state: req.body.state,
+                value: req.body.value,
+                color: req.body.color,
+                year: req.body.year,
+                make: req.body.make,
+                body: req.body.body,
+                model: req.body.model,
+                vin: req.body.vin,
+                owner: req.body.owner,
+            }
+        );
+
+        if (!errors.isEmpty()) {
+            
+            res.render('vehicle_form', { title: 'Create Vehicle', vehicle: vehicle, errors: errors.array() });
+            return;
+        }
+        else {
+            
+            vehicle.save(function (err) {
+                if (err) { return next(err); }
+                
+                res.redirect(vehicle.url);
+            });
+        }
+    }
+];
